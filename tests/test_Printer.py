66c4891a06d2ba6979b4af71108213061babcc5a
DarkWinged <james.levi.rogers@gmail.com>
2023-09-22 10:47:24 -0500

    Updaed printer interface tests AB#536

--- tests/test_Printer.py
+++ tests/test_Printer.py
@@ -4,15 +4,19 @@ class TestPrinter(Printer):
     def __init__(self, output: callable) -> None:
         self.output = output
         self.content = ''
+        self.file = None
+        self.flush = False
 
     def print(self, *objects: list[str], sep: str=' ', end: str='\n', file=None, flush: bool=False) -> None:
         self.content += sep.join(objects) + end
+        self.file = file
+        self.flush = flush
 
     def clear(self) -> None:
         self.content = ''
 
     def update(self) -> None:
-        self.output(self.content)
+        self.output(self.content, end='', file = self.file, flush = self.flush)
         self.clear()
 
 
@@ -55,7 +59,7 @@ def test_pinter_clear() -> None:
 
 def test_pinter_update(capsys) -> None:
     printer = TestPrinter(print)
-    printer.content = 'test'
+    printer.content = 'test\n'
     printer.update()
     captured = capsys.readouterr()
-    assert captured.out.strip() == 'test'
+    assert captured.out == 'test\n'
