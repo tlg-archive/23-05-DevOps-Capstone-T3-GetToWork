725410c0be8778b68b55b5de0408e94eaf14ff13
dephillips98 <dephillips@gmail.com>
2023-09-22 14:09:11 -0500

    add title AB#538

--- gui.py
+++ gui.py
@@ -1,15 +1,17 @@
 from tkinter import *
 from GetToWork import *
-#from PIL import Image, ImageTk
+from PIL import Image, ImageTk
 
 #tkinter.Tk(screenName="game", baseName=None, className='Tk', useTk=True, sync=False, use=None)
 root = Tk()
-#with open("json/title.txt", "r") as title:
- #   title = title.read()
+#with open("json/title2.png", "r") as title:
+#    title = title.read()
 #    print(title)
-#image = Image.open("json/title2.png")
-#photo = ImageTk.PhotoImage("Game")
-#root.title(photo)
-my_label = Label(root, text="game sample")
+image = Image.open("json/title2.png")
+photo = ImageTk.PhotoImage(image)
+root.title(photo)
+my_label = Label(root, image=photo)
+
+                 
 my_label.pack()
 root.mainloop()
