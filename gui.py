e9c64a0ed24c8c22c612fce84389c3bcf8dd61f6
dephillips98 <dephillips@gmail.com>
2023-09-22 13:56:35 -0500

    created window AB#534

---
+++ gui.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from GetToWork import *
+from PIL import Image, ImageTk
+
+#tkinter.Tk(screenName="game", baseName=None, className='Tk', useTk=True, sync=False, use=None)
+root = Tk()
+#with open("json/title.txt", "r") as title:
+#    title = title.read()
+#    print(title)
+#image = Image.open("json/title2.png")
+#photo = ImageTk.PhotoImage("Game")
+#root.title(photo)
+#my_label = Label(root, image=photo)
+#xsmy_label.pack()
+root.mainloop()
